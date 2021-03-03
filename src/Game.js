74e2d04e96a38a3ee607f527e3e25f1e6d93c9af
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-03 16:13:39 +0100

    changed socket from localhost to online server

--- src/Game.js
+++ src/Game.js
@@ -13,8 +13,8 @@ import CreateGameScreen from './Componets/CreateGameScreen'
 
 import Board from './Componets/Board'
 
-//const socket = io.connect("https://limitless-shelf-54190.herokuapp.com");
-const socket = io("http://localhost:4000",  { autoConnect: true } );
+const socket = io.connect("https://limitless-shelf-54190.herokuapp.com");
+//const socket = io("http://localhost:4000",  { autoConnect: true } );
 
 socket.emit('test')
 
