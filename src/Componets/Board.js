43465d0df24ef8dd60854f4d8610c7106959d18f
Jesper Gustafsson <jesper95gustafsson@gmail.com>
2021-03-06 15:45:02 +0100

    changed square colors

--- src/Componets/Board.js
+++ src/Componets/Board.js
@@ -71,7 +71,7 @@ const HorizontalAxisSquare = styled.div`
            {
           row.map((col, indexX) => {
             let squareColor = (col.x%2===1 && col.y%2===0) || (col.x%2===0 && col.y%2===1) ? lightSquareColor : darkSquareColor
-            if (player === 2) squareColor = (col.x%2===1 && col.y%2===0) || (col.x%2===0 && col.y%2===1) ? darkSquareColor : lightSquareColor
+           // if (player === 2) squareColor = (col.x%2===1 && col.y%2===0) || (col.x%2===0 && col.y%2===1) ? darkSquareColor : lightSquareColor
             return (<Square color = {col.player === 1 ? 'white' : 'black' } selected = {col.selected} bgc={squareColor} onSelectPiece={onSelectPiece} position = {[col.x,col.y]} graphic={`${col.graphic}`}></Square>)
           })
             }
